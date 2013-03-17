e222d1d183d3fd52adb1b120be3316fe8bb38d0e
Thor Åge Eldby <thoraageeldby@gmail.com>
2013-03-17 18:21:21 +0100

    Created throwables demo

---
+++ src/test/java/no/arktekk/talkjdk8/ThrowablesDemo.java
@@ -0,0 +1,18 @@
+package no.arktekk.talkjdk8;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ThrowablesDemo {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(FileUtils.readFileToString(new File(args[0])));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
