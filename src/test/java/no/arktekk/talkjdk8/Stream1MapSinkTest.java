2177b33a50fb0e161f266d38cfa5484b880e10a7
Thor Åge Eldby <thoraageeldby@gmail.com>
2013-03-18 22:35:31 +0100

    Spelling error; all letters in for that is

--- src/test/java/no/arktekk/talkjdk8/Stream1MapSinkTest.java
+++ src/test/java/no/arktekk/talkjdk8/Stream1MapSinkTest.java
@@ -13,8 +13,8 @@ public class Stream1MapSinkTest {
     @Test
     public void test() {
         List<String> names = asList("Tom Waits", "Johnny Cash", "Stefan Sundström");
-        List<String> surNames = names.stream().map((String s) -> s.substring(0, s.indexOf(' '))).collect(Collectors.<String>toList());
-        assertEquals(asList("Tom", "Johnny", "Stefan"), surNames);
+        List<String> foreNames = names.stream().map((String s) -> s.substring(0, s.indexOf(' '))).collect(Collectors.<String>toList());
+        assertEquals(asList("Tom", "Johnny", "Stefan"), foreNames);
     }
 
 }
